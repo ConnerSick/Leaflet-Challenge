62ccd85ca79cbbfa7e081a2db6fb90cc38aff77a
Conner <64811824+ConnerSick@users.noreply.github.com>
2020-09-18 15:32:24 -0700

    update

--- Leafleat-Step-1/static/js/logic.js
+++ Leafleat-Step-1/static/js/logic.js
@@ -20,12 +20,8 @@ var graymap = L.tileLayer(
   
   graymap.addTo(map);
   
-  // Here we make an AJAX call that retrieves our earthquake geoJSON data.
   d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_month.geojson", function(data) {
   
-    // This function returns the style data for each of the earthquakes we plot on
-    // the map. We pass the magnitude of the earthquake into two separate functions
-    // to calculate the color and radius.
     function styleInfo(feature) {
       return {
         opacity: 1,
@@ -38,7 +34,6 @@ var graymap = L.tileLayer(
       };
     }
   
-    // This function determines the color of the marker based on the magnitude of the earthquake.
     function getColor(depth) {
       switch (true) {
       case depth > 90:
@@ -56,25 +51,17 @@ var graymap = L.tileLayer(
       }
     }
   
-    // This function determines the radius of the earthquake marker based on its magnitude.
-    // Earthquakes with a magnitude of 0 were being plotted with the wrong radius.
+
     function getRadius(magnitude) {
-      if (magnitude === 0) {
-        return 1;
-      }
-  
-      return magnitude * 4;
+      return magnitude * 3;
     }
   
-    // Here we add a GeoJSON layer to the map once the file is loaded.
     L.geoJson(data, {
-      // We turn each feature into a circleMarker on the map.
       pointToLayer: function(feature, latlng) {
         return L.circleMarker(latlng);
       },
-      // We set the style for each circleMarker using our styleInfo function.
+      
       style: styleInfo,
-      // We create a popup for each marker to display the magnitude and location of the earthquake after the marker has been created and styled
       onEachFeature: function(feature, layer) {
         layer.bindPopup(
           "Magnitude: "
@@ -87,12 +74,10 @@ var graymap = L.tileLayer(
       }
     }).addTo(map);
   
-    // Here we create a legend control object.
     var legend = L.control({
       position: "bottomright"
     });
   
-    // Then add all the details for the legend
     legend.onAdd = function() {
       var div = L.DomUtil.create("div", "info legend");
   
@@ -106,7 +91,6 @@ var graymap = L.tileLayer(
         "#ea2c2c"
       ];
   
-      // Looping through our intervals to generate a label with a colored square for each interval.
       for (var i = 0; i < grades.length; i++) {
         div.innerHTML += "<i style='background: " + colors[i] + "'></i> "
         + grades[i] + (grades[i + 1] ? "&ndash;" + grades[i + 1] + "<br>" : "+");
@@ -114,7 +98,6 @@ var graymap = L.tileLayer(
       return div;
     };
   
-    // Finally, we our legend to the map.
     legend.addTo(map);
   });
   
